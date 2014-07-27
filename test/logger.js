810c3c23e5fa894d3aad1d489d0024acd6f07105
Aurelio A. Heckert <aurium@gmail.com>
2014-07-26 23:14:10 -0300

    logger correction

--- test/logger.js
+++ test/logger.js
@@ -4,12 +4,46 @@ var logFactory = require('../lib/logger');
 var sinon = require('sinon');
 var log = logFactory('test');
 var fs = require('fs');
-var mockFS;
 
 describe('Logger', function() {
 
-  beforeEach(function() { mockFS = sinon.mock(fs) });
-  afterEach(function () { mockFS.restore() });
+  beforeEach(function() {
+    sinon.stub(fs, 'writeFile');
+  });
+
+  afterEach(function () {
+    fs.writeFile.restore();
+  });
+
+  it('should log', function() {
+    logFactory.setWriteTo('/dev/null');
+    log('testing');
+    sinon.assert.calledOnce(fs.writeFile);
+    sinon.assert.calledWithMatch(fs.writeFile, '/dev/null', 'testing');
+    
+  });
+
+  it('should log multiple args', function() {
+    logFactory.setWriteTo('/dev/null');
+    logFactory.setUseColors(false);
+    log('testing', 123);
+    sinon.assert.calledWithMatch(fs.writeFile, '/dev/null', 'testing | 123');
+  });
+
+  it('should log multiple complex args', function() {
+    logFactory.setWriteTo('/dev/null');
+    logFactory.setUseColors(false);
+    log([11,22,33], {a:true, b:false});
+    sinon.assert.calledWithMatch(fs.writeFile,
+      '/dev/null', /11, *22, *33.*\|.*a: *true, *b: *false/);
+  });
+
+  it('should log multiple args with color', function() {
+    logFactory.setWriteTo('/dev/null');
+    logFactory.setUseColors(true);
+    log('testing', 123);
+    sinon.assert.calledWithMatch(fs.writeFile, '/dev/null', /testing | .*123/);
+  });
 
   it('should log to STDIO', function() {
     logFactory.setWriteTo('STDIO');
@@ -18,13 +52,6 @@ describe('Logger', function() {
     log('testing');
     mockConsole.verify();
     mockConsole.restore();
-  });
-
-  it('should log to a file', function() {
-    logFactory.setWriteTo('/somedir/somefile.log');
-    mockFS.expects('writeFile').once();
-    log('testing');
-    mockFS.verify();
   });
 
 });
