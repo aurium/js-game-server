4d806d63c1a08e1f6481ad7695e1f71735c3c0cb
Aurélio A. Heckert <aurelio@colivre.coop.br>
2014-08-03 16:12:05 -0300

    Encapsulate all game related querying

--- test/game.js
+++ test/game.js
@@ -1,14 +1,21 @@
 'use strict';
 
 require('should');
-
+var path = require('path');
 var Game = require('../lib/game');
 
 describe('Game module', function() {
 
   it('should load configuration', function() {
-    var game = new Game('../examples/some-game');
+    var game = new Game('examples/some-game');
     game.should.have.property('port').greaterThan(3000);
+    game.config.name.should.be.equal('some-game');
+  });
+
+  it('should find main file', function() {
+    var game = new Game('examples/some-game');
+    var dataDir = path.resolve('examples/some-game');
+    game.main.should.be.equal(path.join(dataDir,'game/index.js'));
   });
 
 });
