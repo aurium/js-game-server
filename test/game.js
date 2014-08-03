fbe28f6b920aef59b7736596480a81dc6372113c
Aurélio A. Heckert <aurelio@colivre.coop.br>
2014-08-03 17:36:35 -0300

    Support poorly configured game dir

--- test/game.js
+++ test/game.js
@@ -6,10 +6,20 @@ var Game = require('../lib/game');
 
 describe('Game module', function() {
 
-  it('should load configuration', function() {
+  it('should load configuration from well configured game', function() {
     var game = new Game('examples/some-game');
     game.should.have.property('port').greaterThan(3000);
-    game.config.name.should.be.equal('some-game');
+    game.pakConfig.name.should.be.equal('some-game');
+    game.gameConfig.title.should.be.equal('Some Game');
+    game.codeName.should.be.equal('some-game');
+  });
+
+  it('should load configuration from non configured game', function() {
+    var game = new Game('examples/some-other-game');
+    game.should.have.property('port').greaterThan(3000);
+    game.pakConfig.name.should.be.equal('some-other-game');
+    game.gameConfig.title.should.be.equal('some-other-game');
+    game.codeName.should.be.equal('some-other-game');
   });
 
   it('should find main file', function() {
