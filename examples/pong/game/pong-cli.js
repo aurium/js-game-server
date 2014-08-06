55cc24c433ac5249ed54bf3806c330d1a05fae48
Aurélio A. Heckert <aurelio@colivre.coop.br>
2014-08-06 03:32:52 -0300

    pong events and moviment

--- examples/pong/game/pong-cli.js
+++ examples/pong/game/pong-cli.js
@@ -1,24 +1,50 @@
 'use strict';
 
 var socket = {};
+var config = {};
+var connected = false;
 
 function connect() {
+  connected = true;
   console.log('connecting');
   if (!socket.connected) socket = io(document.location.href);
+  socket.on('news', onNews);
+  socket.on('config', onConfig);
+  socket.on('disconnect', onDisconnect);
+  setTimeout(tic, 10);
+}
 
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('usrCmd', { cmd: 'hi' });
-  });
+function tic() {
+  if (!connected) return;
+  pad1.style.top  = (100 * config.pad1)  + '%';
+  pad2.style.top  = (100 * config.pad2)  + '%';
+  ball.style.top  = (100 * config.ballY) + '%';
+  ball.style.left = (100 * config.ballX) + '%';
+  setTimeout(tic, 33);
+};
 
-  socket.on('disconnect', function (data) {
-    console.log('disconnected', data);
-    openDialog(
-      'Disconnected', 'Do you want to reconnect?',
-      'Reconnect', function() {
-        document.location.reload();
-        dialog.style.display = 'none';
-    });
+function onNews(data) {
+  console.log(data);
+  var msg = document.createElement('p');
+  msg.className = 'show';
+  msg.innerHTML = data.message;
+  messageBox.appendChild(msg);
+  setTimeout(function(){ msg.className = '' }, 10);
+  setTimeout(function(){ messageBox.removeChild(msg) }, 20*1000);
+}
+
+function onConfig(data) {
+  for (var k in data) { config[k] = data[k] }
+}
+
+function onDisconnect(data) {
+  connected = false;
+  console.log('disconnected', data);
+  openDialog(
+    'Disconnected', 'Do you want to reconnect?',
+    'Reconnect', function() {
+      document.location.reload();
+      dialog.style.display = 'none';
   });
 }
 
@@ -31,3 +57,13 @@ function openDialog(title, content, btLabel, btFunc) {
   dialogBtFunc.onclick = btFunc;
   console.log('open dialog done');
 }
+
+game.onclick = function(ev) {
+  console.log('click',ev);
+  if (!connected) return;
+  socket.emit('usrCmd', {
+    cmd: 'click',
+    y: ev.clientY/game.clientHeight
+  });
+};
+
--- examples/pong/game/pong-srv.js
+++ examples/pong/game/pong-srv.js
@@ -14,11 +14,27 @@ function Game(player1, player2) {
   this.players = [player1, player2];
   player1.joinGame(this);
   player2.joinGame(this);
-  io.to(this.id).emit('news', { hello: 'world',
-    player1: player1.name,
-    player2: player2.name
-  });
+  io.to(this.id)
+    .emit('news', { message: 'You found a pair to play' })
+    .emit('config', { player1: player1.name, player2: player2.name });
+  setTimeout(this.tic.bind(this), 1000);
+  this.ball = { x:0.5, y:0.5 };
 }
+
+Game.prototype.tic = function() {
+  if (!games[this.id]) return;
+  this.ball.x += 0.01;
+  this.ball.y += 0.01;
+  this.players[0].padY = (this.players[0].padY + this.players[0].gotoY) / 2;
+  this.players[1].padY = (this.players[1].padY + this.players[1].gotoY) / 2;
+  io.to(this.id).emit('config', {
+    pad1: this.players[0].padY,
+    pad2: this.players[1].padY,
+    ballX: this.ball.x,
+    ballY: this.ball.y
+  });
+  setTimeout(this.tic.bind(this), 33);
+};
 
 Game.prototype.end = function() {
   this.players[0].exit();
@@ -30,13 +46,23 @@ function Player(socket) {
   this.socket = socket;
   this.name = 'player' + pCounter++;
   socket.on('disconnect', this.onExit.bind(this));
+  this.padY = this.gotoY = 0.5;
 }
 
 Player.prototype.joinGame = function(game) {
   this.game = game;
   this.socket.join(game.id);
-  this.socket.on('usrCmd', function(data){ log.debug('usrCmd',data) });
+  this.socket.on('usrCmd', this.onCmd.bind(this));
   this.socket.emit('news', { message: 'My key', key:this.whoInTheGame().me.key });
+};
+
+Player.prototype.onCmd = function(data){
+  log.debug('usrCmd', data);
+  if (data.cmd=='click') this.onClick(data);
+};
+
+Player.prototype.onClick = function(data){
+  this.gotoY = data.y;
 };
 
 Player.prototype.whoInTheGame = function() {
@@ -73,7 +99,7 @@ Player.prototype.exit = function() {
 
 io.on('connection', function(socket) {
   if ( alonePlayer && alonePlayer.id != socket.id ) {
-    socket.emit('news', { message: 'Entering a game...', id:socket.id });
+    socket.emit('news', { message: 'Entering in a game...', id:socket.id });
     new Game(alonePlayer, new Player(socket));
     alonePlayer = null;
   } else {
