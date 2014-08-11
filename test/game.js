7fd18d16449953a015f0c62109dd21e713a27899
Aurélio A. Heckert <aurelio@colivre.coop.br>
2014-08-10 22:03:57 -0300

    Add data persistence feature for the games

--- test/game.js
+++ test/game.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require('should');
+var sh = require('shelljs');
+var fs = require('fs');
 var path = require('path');
 var Game = require('../lib/game');
 
@@ -26,6 +28,34 @@ describe('Game module', function() {
     var game = new Game('examples/some-game');
     var srcDir = path.resolve('examples/some-game/game');
     game.main.should.be.equal(path.join(srcDir,'main.js'));
+  });
+
+  it('should start a db file', function(done) {
+    var gameDir = path.join(sh.tempdir(), 'game.tmp' + Math.random());
+    var dataFile = path.join(gameDir,'data.json');
+    sh.cp('-r', 'examples/some-game', gameDir);
+    sh.rm('-r', dataFile);
+    var game = new Game(gameDir);
+    game.db('number', 42, function() {
+      var data = require(dataFile);
+      data.number.should.be.equal(42);
+      sh.rm('-r', gameDir);
+      done();
+    });
+  });
+
+  it('should use db file', function() {
+    var gameDir = path.join(sh.tempdir(), 'game.tmp' + Math.random());
+    var dataFile = path.join(gameDir,'data.json');
+    sh.cp('-r', 'examples/some-game', gameDir);
+    fs.writeFileSync(dataFile, '{"number": 42}');
+    var game = new Game(gameDir);
+    game.db('number').should.be.equal(42);
+    game.db('phrase', 'So long, and thanks for all the fish');
+    var data = require(dataFile);
+    data.number.should.be.equal(42);
+    data.phrase.should.be.equal('So long, and thanks for all the fish');
+    sh.rm('-r', gameDir);
   });
 
 });
